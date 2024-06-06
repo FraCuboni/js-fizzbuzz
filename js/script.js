3cd9e975512e734b6d5a1d31a940c590883b9a65
FraCuboni <fracuboni@gmail.com>
2024-06-06 16:09:53 +0200

    number printer created

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// creare un programma che stambi in console numeri da 1 a 100 ma che stampi fizz al posto dei numeri divisibili per 3 e buzz al posto dei numeri divisibili per 5  
+
+for (let index = 1; index < 101; index++) {
+
+    console.log(index);
+
+}
+// creo il ciclo che crei i numeri da 1 a 100 e me li stampi in console
+
+// scandisco quali numeri possono essere divisi per 3 
+
+// scandisco quali numeri possono essere divisi per 5
+
+// assegno i valori ai numeri divisibili per 3 e per 5
+
