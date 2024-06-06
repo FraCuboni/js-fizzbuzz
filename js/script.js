dff5d4825dc5fe88d858f0f68f108b33ad412d4d
FraCuboni <fracuboni@gmail.com>
2024-06-06 17:15:05 +0200

    numbers added need to add fuzzbuzz

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 // creo il ciclo che crei i numeri da 1 a 100 e me li stampi in console
 for (let index = 1; index < 101; index++) {
 
-
+    // creo i div square
     const container = document.querySelector("div.container");
     const square = document.createElement("div.square");
     square.classList.add('square')
@@ -15,24 +15,19 @@ for (let index = 1; index < 101; index++) {
         square.classList.add('fizzbuzz')
     }
 
-
     // scandisco quali numeri possono essere divisi per 3 
     else if (index % 3 === 0) {
         square.classList.add('fizz')
 
     }
 
-
     // scandisco quali numeri possono essere divisi per 5
     else if (index % 5 === 0) {
         square.classList.add('buzz')
     }
 
-    
+    square.textContent = index;
 
-    console.log(index);
-
-    // creo i div square
 
 
 
