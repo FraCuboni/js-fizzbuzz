9dc021a203db37fc7f4b2949ffc28c2fc5514191
FraCuboni <fracuboni@gmail.com>
2024-06-06 17:25:39 +0200

    done

--- js/script.js
+++ js/script.js
@@ -13,20 +13,28 @@ for (let index = 1; index < 101; index++) {
     // assegno i valori ai numeri divisibili per 3 e per 5
     if (index % 3 === 0 && index % 5 === 0) {
         square.classList.add('fizzbuzz')
+        square.textContent = "fizzbuzz";
     }
 
     // scandisco quali numeri possono essere divisi per 3 
     else if (index % 3 === 0) {
         square.classList.add('fizz')
+        square.textContent = "fizz";
 
     }
 
     // scandisco quali numeri possono essere divisi per 5
     else if (index % 5 === 0) {
         square.classList.add('buzz')
+        square.textContent = "buzz";
     }
 
-    square.textContent = index;
+    // scrivo il numero se non appartiene alle altre classi prima
+    else{
+        square.textContent = index;
+    }
+
+    // ho aggiunto questo else finale perchè non riuscivo a dare il valore index='fizzbuzz'; in quanto cambiava anche il valore di index
 
 
 
