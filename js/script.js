16bf1751f0e8a804c8e495bdea4df2714c0e87b1
FraCuboni <fracuboni@gmail.com>
2024-06-06 16:13:02 +0200

    %3 & %5 found

--- js/script.js
+++ js/script.js
@@ -1,15 +1,31 @@
 // creare un programma che stambi in console numeri da 1 a 100 ma che stampi fizz al posto dei numeri divisibili per 3 e buzz al posto dei numeri divisibili per 5  
 
+
+// creo il ciclo che crei i numeri da 1 a 100 e me li stampi in console
 for (let index = 1; index < 101; index++) {
 
     console.log(index);
 
-}
-// creo il ciclo che crei i numeri da 1 a 100 e me li stampi in console
-
+    
 // scandisco quali numeri possono essere divisi per 3 
+    if (index % 3 === 0) {
+        console.log(' è un multiplo di 3');
+    }
+
 
 // scandisco quali numeri possono essere divisi per 5
+    if (index % 5 === 0) {
+        console.log(' è un multiplo di 5');
+    }
+
+
 
 // assegno i valori ai numeri divisibili per 3 e per 5
+
+
+
+}
+
+
+
 
