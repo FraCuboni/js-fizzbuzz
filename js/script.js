9405213370075536e1cfc5c36940140b95bbb0d6
FraCuboni <fracuboni@gmail.com>
2024-06-06 17:09:30 +0200

    added colors to the square w classes

--- js/script.js
+++ js/script.js
@@ -4,24 +4,35 @@
 // creo il ciclo che crei i numeri da 1 a 100 e me li stampi in console
 for (let index = 1; index < 101; index++) {
 
-    console.log(index);
+
+    const container = document.querySelector("div.container");
+    const square = document.createElement("div.square");
+    square.classList.add('square')
+    container.appendChild(square);
+    
+    // assegno i valori ai numeri divisibili per 3 e per 5
+    if (index % 3 === 0 && index % 5 === 0) {
+        square.classList.add('fizzbuzz')
+    }
+
+
+    // scandisco quali numeri possono essere divisi per 3 
+    else if (index % 3 === 0) {
+        square.classList.add('fizz')
+
+    }
+
+
+    // scandisco quali numeri possono essere divisi per 5
+    else if (index % 5 === 0) {
+        square.classList.add('buzz')
+    }
 
     
-// assegno i valori ai numeri divisibili per 3 e per 5
-    if (index % 3 === 0 && index % 5 === 0) {
-        console.log('fizzbuz');
-    }
 
-// scandisco quali numeri possono essere divisi per 3 
-    else if (index % 3 === 0) {
-        console.log('fizz');
-    }
+    console.log(index);
 
-
-// scandisco quali numeri possono essere divisi per 5
-    else if (index % 5 === 0) {
-        console.log('buzz');
-    }
+    // creo i div square
 
 
 
